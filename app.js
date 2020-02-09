cdce40336cadbc409245b96b8b3053af1de08fd9
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-09 14:16:09 +0300

    api call

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const members = require("./Members.js");
 const logger = require("./middleware/logger.js");
+const members = require("./Members.js");
 
 const app = express();
 
-app.use(logger.logger);
-
-app.get("/api/members", (req, res) => {
-  res.json(members.data);
-});
-
-app.get("/api/members/:name", (req, res) => {
-  res.json(members.data.filter(item => item.name === req.params.name));
-});
-
 app.use(express.static(path.join(__dirname, "public")));
 
+//API call
+{
+  app.get("/api/members", (req, res) => {
+    res.json(members.data);
+  });
+
+  app.get("/api/members/:name", (req, res) => {
+    res.json(members.data.filter(item => item.name === req.params.name));
+  });
+}
+//
+app.use(logger.logger);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
